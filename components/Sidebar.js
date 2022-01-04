2f170ad85efe57104da4e5e1448780be86ad4374
Gyozadaigaku <gaspardcube15@gmail.com>
2022-01-04 15:39:54 +0900

    convert rcc to rfc

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -1,65 +1,31 @@
-import React from 'react'
+import React, { useState } from 'react'
 import FullCalendar, { formatDate } from '@fullcalendar/react'
 import dayGridPlugin from '@fullcalendar/daygrid'
 import timeGridPlugin from '@fullcalendar/timegrid'
 import interactionPlugin from '@fullcalendar/interaction'
 import { Box } from '@chakra-ui/react'
 
-export default class DemoApp extends React.Component {
-  state = {
-    weekendsVisible: true,
-    currentEvents: [],
-  }
-
-  render() {
-    return (
-      <Box minW="400px" px={2} py={8}>
-        <FullCalendar
-          plugins={[dayGridPlugin, timeGridPlugin, interactionPlugin]}
-          headerToolbar={{
-            left: 'title',
-            center: '',
-            right: 'prev,today,next',
-          }}
-          initialView="dayGridMonth"
-          editable={true}
-          selectable={true}
-          selectMirror={true}
-          dayMaxEvents={true}
-          weekends={this.state.weekendsVisible}
-          // initialEvents={INITIAL_EVENTS} // alternatively, use the `events` setting to fetch from a feed
-          select={this.handleDateSelect}
-          eventContent={renderEventContent} // custom render function
-          eventClick={this.handleEventClick}
-          eventsSet={this.handleEvents} // called after events are initialized/added/changed/removed
-          /* you can update a remote database when these fire:
-            eventAdd={function(){}}
-            eventChange={function(){}}
-            eventRemove={function(){}}
-            */
-        />
-        {this.renderSidebar()}
-      </Box>
-    )
-  }
+const Calendar = () => {
+  const [weekendsVisible, setWeekendsVisible] = useState(true)
+  const [currentEvents, setCurrentEvents] = useState([])
 
   /*
   Select dates and you will be prompted to create a new event
   Drag, drop, and resize events
   Click an event to delete it
   */
-  renderSidebar() {
+  const renderSidebar = () => {
     return (
       <div className="demo-app-sidebar">
         <div className="demo-app-sidebar-section">
-          <h2>All Events ({this.state.currentEvents.length})</h2>
-          <ul>{this.state.currentEvents.map(renderSidebarEvent)}</ul>
+          <h2>All Events ({currentEvents.length})</h2>
+          <ul>{currentEvents.map(renderSidebarEvent)}</ul>
         </div>
       </div>
     )
   }
 
-  handleDateSelect = (selectInfo) => {
+  const handleDateSelect = (selectInfo) => {
     let title = prompt('Please enter a new title for your event')
     let calendarApi = selectInfo.view.calendar
 
@@ -76,20 +42,44 @@ export default class DemoApp extends React.Component {
     }
   }
 
-  handleEventClick = (clickInfo) => {
+  const handleEventClick = (clickInfo) => {
     if (confirm(`Are you sure you want to delete the event '${clickInfo.event.title}'`)) {
       clickInfo.event.remove()
     }
   }
 
-  handleEvents = (events) => {
-    this.setState({
-      currentEvents: events,
-    })
-  }
+  return (
+    <Box minW="400px" px={2} py={8}>
+      <FullCalendar
+        plugins={[dayGridPlugin, timeGridPlugin, interactionPlugin]}
+        headerToolbar={{
+          left: 'title',
+          center: '',
+          right: 'prev,today,next',
+        }}
+        initialView="dayGridMonth"
+        editable={true}
+        selectable={true}
+        selectMirror={true}
+        dayMaxEvents={true}
+        weekends={weekendsVisible}
+        // initialEvents={INITIAL_EVENTS} // alternatively, use the `events` setting to fetch from a feed
+        select={handleDateSelect}
+        eventContent={renderEventContent} // custom render function
+        eventClick={handleEventClick}
+        eventsSet={(e) => setCurrentEvents(e)} // called after events are initialized/added/changed/removed
+        /* you can update a remote database when these fire:
+            eventAdd={function(){}}
+            eventChange={function(){}}
+            eventRemove={function(){}}
+            */
+      />
+      {renderSidebar()}
+    </Box>
+  )
 }
 
-function renderEventContent(eventInfo) {
+const renderEventContent = (eventInfo) => {
   return (
     <>
       <b>{eventInfo.timeText}</b>
@@ -98,7 +88,7 @@ function renderEventContent(eventInfo) {
   )
 }
 
-function renderSidebarEvent(event) {
+const renderSidebarEvent = (event) => {
   return (
     <li key={event.id}>
       <b>{formatDate(event.start, { year: 'numeric', month: 'short', day: 'numeric' })}</b>
@@ -106,3 +96,5 @@ function renderSidebarEvent(event) {
     </li>
   )
 }
+
+export default Calendar
