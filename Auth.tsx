bab3e0731100986edee702f252022e09635371b0
Gyozadaigaku <gaspardcube15@gmail.com>
2022-01-13 02:46:46 +0900

    define the type

--- Auth.tsx
+++ Auth.tsx
@@ -1,9 +1,7 @@
 const AuthContext = createContext({})
 import { createContext, useState, useEffect, useContext } from 'react'
 import { getAuth } from 'firebase/auth'
-// @ts-expect-error ts-migrate(2307) FIXME: Cannot find module './components/Loading' or its c... Remove this comment to see the full error message
 import Loading from './components/Loading'
-// @ts-expect-error ts-migrate(2307) FIXME: Cannot find module './components/Login' or its cor... Remove this comment to see the full error message
 import Login from './components/Login'
 import nookies from 'nookies'
 
@@ -20,22 +18,18 @@ export const AuthProvider = ({ children }: any) => {
         return
       }
       const token = await user.getIdToken()
-      // @ts-expect-error ts-migrate(2345) FIXME: Argument of type 'User' is not assignable to param... Remove this comment to see the full error message
       setCurrentUser(user)
       setLoading(false)
       nookies.set(undefined, 'token', token, {})
     })
   }, [])
   if (loading) {
-    // @ts-expect-error ts-migrate(17004) FIXME: Cannot use JSX unless the '--jsx' flag is provided... Remove this comment to see the full error message
     return <Loading />
   }
 
   if (!currentUser) {
-    // @ts-expect-error ts-migrate(17004) FIXME: Cannot use JSX unless the '--jsx' flag is provided... Remove this comment to see the full error message
     return <Login />
   } else {
-    // @ts-expect-error ts-migrate(17004) FIXME: Cannot use JSX unless the '--jsx' flag is provided... Remove this comment to see the full error message
     return <AuthContext.Provider value={{ currentUser }}>{children}</AuthContext.Provider>
   }
 }
