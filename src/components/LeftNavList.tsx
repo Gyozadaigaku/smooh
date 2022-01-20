262c60581932ee5af8da1a2298ee9c048becdb3a
Gyozadaigaku <gaspardcube15@gmail.com>
2022-01-21 02:12:37 +0900

    add tags page

--- src/components/LeftNavList.tsx
+++ src/components/LeftNavList.tsx
@@ -1,25 +1,30 @@
-import { Wrap, WrapItem, Avatar, IconButton, Flex, Text, Button, VStack, Box, StackDivider, Icon, Divider } from '@chakra-ui/react'
 import { CalendarIcon } from '@chakra-ui/icons'
-import { FaInbox, FaRegListAlt } from 'react-icons/fa'
 import { CgTag } from 'react-icons/cg'
+import { FaInbox, FaRegListAlt } from 'react-icons/fa'
+import { Wrap, WrapItem, Avatar, IconButton, Flex, Text, Button, VStack, Box, StackDivider, Icon, Divider } from '@chakra-ui/react'
+import Link from 'next/link'
 
 const LeftNavList = () => {
   const navItems = [
     {
       icon: FaInbox,
       label: 'Inbox',
+      path: '/',
     },
     {
       icon: CalendarIcon,
       label: 'Schedule',
+      path: '/schedule',
     },
     {
       icon: FaRegListAlt,
       label: 'List',
+      path: '/list',
     },
     {
       icon: CgTag,
       label: 'Tags',
+      path: '/tags',
     },
   ]
 
@@ -30,10 +35,14 @@ const LeftNavList = () => {
         <Box m="auto" width="100%" textAlign="center">
           {navItems.map((navItems, id) => {
             return (
-              <Button justifyContent="left" variant="ghost" width="95%" px={2} py={6} my={1} key={id}>
-                <Icon as={navItems.icon} boxSize={6} />
-                <Text pl={4}>{navItems.label}</Text>
-              </Button>
+              <Link key={id} href={navItems.path}>
+                <a>
+                  <Button justifyContent="left" variant="ghost" width="95%" px={2} py={6} my={1} key={id}>
+                    <Icon as={navItems.icon} boxSize={6} />
+                    <Text pl={4}>{navItems.label}</Text>
+                  </Button>
+                </a>
+              </Link>
             )
           })}
         </Box>
---
+++ src/pages/tags.tsx
@@ -0,0 +1,97 @@
+import { AddIcon } from '@chakra-ui/icons'
+import { collection, orderBy, query, getDocs } from '@firebase/firestore'
+import { db } from '../firebase'
+import { IconButton, useDisclosure, Box, Flex, Heading } from '@chakra-ui/react'
+import { TodoContext } from './TodoContext'
+import { useAuth } from '../Auth'
+import { useState } from 'react'
+import { verifyIdToken } from '../firebaseAdmin'
+import nookies from 'nookies'
+import LeftNavList from '../components/LeftNavList'
+import ProfileMenu from '../components/ProfileMenu'
+import React from 'react'
+import Sidebar from '../components/Sidebar'
+import TodoForm from '../components/TodoForm'
+import TodoList from '../components/TodoList'
+
+const Tags = ({ todosProps }: any) => {
+  const { currentUser } = useAuth()
+  const { isOpen, onOpen, onClose } = useDisclosure()
+
+  const [todo, setTodo] = useState({ title: '', isCompleted: false, startDate: new Date(), endDate: new Date(), tags: [] })
+
+  console.log('tags.tsx')
+  console.log('todosProps:', todosProps)
+  console.log('todo:', todo)
+
+  return (
+    <TodoContext.Provider value={{ todo, setTodo }}>
+      <Flex minH="100vh" m="auto">
+        <Flex direction="column" minW={372} w={372} px={2} pt={4} pb={4}>
+          <Sidebar />
+          <LeftNavList />
+          <ProfileMenu currentUser={currentUser} />
+        </Flex>
+        <Box flex={1} px={12} py={4} bg="gray.900">
+          <Heading>Tags</Heading>
+          <TodoForm isOpen={isOpen} onOpen={onOpen} onClose={onClose} />
+          <TodoList todosProps={todosProps} handleToggleModal={onOpen} />
+          <IconButton
+            aria-label="Create task"
+            isFullWidth="true"
+            border="none"
+            variant="outline"
+            color="gray"
+            justifyContent="start"
+            pl={3}
+            mt={5}
+            icon={<AddIcon />}
+            onClick={onOpen}
+          />
+          <IconButton
+            aria-label="Create task"
+            pos="fixed"
+            bottom={8}
+            right={8}
+            colorScheme="blue"
+            bg="blue.400"
+            borderRadius="50%"
+            size="lg"
+            p={0}
+            onClick={onOpen}
+          >
+            <AddIcon w={6} h={6} />
+          </IconButton>
+        </Box>
+      </Flex>
+    </TodoContext.Provider>
+  )
+}
+
+export default Tags
+
+export async function getServerSideProps(context: any) {
+  try {
+    const cookies = nookies.get(context)
+    const token = await verifyIdToken(cookies.token)
+    const { email } = token
+    const collectionRef = collection(db, 'todos')
+    const q = query(
+      collectionRef,
+      // where("email", "==", currentUser?.email),
+      orderBy('timestamp', 'desc'),
+    )
+    const querySnapshot = await getDocs(q)
+    let todos: any = []
+    querySnapshot.forEach((doc) => {
+      todos.push({ ...doc.data(), id: doc.id, timestamp: doc.data().timestamp.toDate().getTime() })
+    })
+    return {
+      props: {
+        todosProps: JSON.stringify(todos) || [],
+      },
+    }
+  } catch (error) {
+    return { props: {} }
+  }
+}
